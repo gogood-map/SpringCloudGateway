860669703e8f1c39602fa022d1bd20cee76f23a0
JohnVSCordeiro <joao.cordeiro@sptech.school>
2024-03-07 19:38:15 -0300

    subindo aplicação

--- src/main/java/com/unir/gateway/Home.java
+++ src/main/java/com/unir/gateway/Home.java
@@ -1,8 +1,7 @@
-package rascunho.rascunho.controller;
+package com.unir.gateway;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
 
 @Controller
 public class Home {
---
+++ src/main/java/com/unir/gateway/Localizacao.java
@@ -0,0 +1,11 @@
+package com.unir.gateway;
+
+import lombok.Data;
+
+@Data
+public class Localizacao {
+
+    private int id;
+    private Double latitude;
+    private Double longitude;
+}
